d19a4988fbca0efd5e8f21541147d38aa1f281ac
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-03-14 12:59:35 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Example from './components/Example';
+
+function App() {
+	return (
+		<div className="App">
+			Hello Worlds
+
+			<Example />
+		</div>
+	);
+}
+
+export default App;
+
