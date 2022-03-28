e81b3273156fd3fdf3af631498ceb1a94abcdaf3
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-03-28 13:36:12 +0200

    Added styling

--- src/components/Vehicle/VehicleCard.js
+++ src/components/Vehicle/VehicleCard.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Card, CardContent, CardMedia } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-export default function VehicleCard(props) {
+export default function VehicleCard({ vehicle }) {
 	const navigate = useNavigate();
 
 	return (
-		<div>
-			<Card onClick={() => { navigate(`/vehicles/detail/${props.vehicle.vehicle_id}`) }}>
-				<CardMedia
-					component="img"
-					height={"100"}
-					image={"https://freepikpsd.com/file/2019/10/car-drawing-png-8-1.png"}
-				/>
-				<CardContent>
-					Title
-					<br />
-					{props.vehicle.vehicle_id}
-					<br />
-					{props.vehicle.brand_name}
+		<Grid item xs={2} sm={4} md={4}>
+			<Card onClick={() => { navigate(`/vehicles/detail/${vehicle.vehicle_id}`) }}>
+				<CardActionArea>
+					<CardMedia
+						component="img"
+						height="200"
+						image={"https://freepikpsd.com/file/2019/10/car-drawing-png-8-1.png"}
+					/>
+					<CardContent>
+						<Typography variant="h5" component="div">
+							{vehicle.brand_name} - {vehicle.vehicle_model}
+						</Typography>
+						<Typography variant="body2" color="text.secondary">
+							LEASEPRIJS VANAF €{vehicle.vehicle_price} P/M
+						</Typography>
 
-				</CardContent>
+
+					</CardContent>
+				</CardActionArea>
 			</Card>
-		</div>
+		</Grid>
 	);
 }
 
--- src/components/Vehicle/Vehicles.js
+++ src/components/Vehicle/Vehicles.js
@@ -1,34 +1,35 @@
 import * as React from 'react';
 import VehicleCard from "./VehicleCard";
 import { useFetch } from "../../hooks/useFetch";
+import { Grid } from '@mui/material';
 
 export default function Vehicles() {
 	const { data, error, loading } = useFetch("http://localhost:8080/vehicle");
 
 	if (loading) {
 		return (
-			<div>
-				...loading
-			</div>
+			<>
+				Loading
+			</>
 		)
 	}
 	if (error) {
 		return (
-			<div>
-				...something went wrong
-			</div>
+			<>
+				Something went wrong while loading
+			</>
 		)
 	}
-
+	console.log(data)
 	return (
-		<>
-			<div className={"vehicle"}>
-				<div >
-					{data.map((item, idx) =>
+		<div className={"vehicle"}>
+			<div >
+				<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+					{data.map((item, idx) => (
 						<VehicleCard vehicle={item} key={idx} />
-					)}
-				</div>
+					))}
+				</Grid>
 			</div>
-		</>
+		</div>
 	)
 }
