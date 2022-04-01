4ecc3c8f7162d88838a7cfdc639f759fb8b62ccd
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-04-01 11:56:20 +0200

    Added form

--- src/components/Vehicle/VehicleDetail.js
+++ src/components/Vehicle/VehicleDetail.js
@@ -1,3 +1,4 @@
+import { Button, FormControl, FormGroup, FormLabel, Grid, TextField } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 
@@ -20,18 +21,37 @@ export default function VehicleDetail() {
 		)
 	}
 
+	console.log(data)
+
 	return (
 		<>
-			<div className={"vehicle"}>
-				<div style={{ height: 400, width: "100%" }}>
-					{data.brand.name} {data.model}
-					<div>
-						{data.colors.map((color, idx) => {
-							return (<li key={idx}>{color}</li>)
-						})}
-					</div>
-				</div>
-			</div>
+			<Grid container spacing={2}>
+				<Grid item xs={5}>
+					Auto hier
+				</Grid>
+				<Grid item xs={7}>
+					Form hier
+					<form>
+						<FormControl>
+							<FormGroup>
+								<TextField label="First name" type="text" name="first_name" variant="standard" required />
+								<TextField label="Last name" type="text" name="last_name" variant="standard" required />
+								<TextField label="Email" type="email" name="email" variant="standard" required />
+								<TextField label="Phone number" type="text" name="phone_number" variant="standard" required />
+							</FormGroup>
+
+							<FormGroup>
+								<TextField label="Street name" type="text" name="street" variant="standard" required />
+								<TextField label="House nr" type="text" name="house_number" variant="standard" required />
+								<TextField label="Zip code" type="text" name="zip_code" variant="standard" required />
+								<TextField label="Provice" type="text" name="province" variant="standard" required />
+								<TextField label="City" type="text" name="city" variant="standard" required />
+								<TextField label="Country" type="text" name="country" variant="standard" required />
+							</FormGroup>
+						</FormControl>
+					</form>
+				</Grid>
+			</Grid>
 		</>
 	)
 }
