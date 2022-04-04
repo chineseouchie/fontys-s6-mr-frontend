93ca0028fee9de5da093e977e5ea2ef8931ff415
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-04-04 14:59:33 +0200

    Added button

--- src/components/Vehicle/VehicleDetail.js
+++ src/components/Vehicle/VehicleDetail.js
@@ -1,4 +1,4 @@
-import { Button, FormControl, FormGroup, FormLabel, Grid, TextField } from "@mui/material";
+import { Button, FormGroup, Grid, TextField } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 
@@ -26,30 +26,37 @@ export default function VehicleDetail() {
 	return (
 		<>
 			<Grid container spacing={2}>
-				<Grid item xs={5}>
-					Auto hier
-				</Grid>
+
 				<Grid item xs={7}>
 					Form hier
 					<form>
-						<FormControl>
-							<FormGroup>
-								<TextField label="First name" type="text" name="first_name" variant="standard" required />
-								<TextField label="Last name" type="text" name="last_name" variant="standard" required />
-								<TextField label="Email" type="email" name="email" variant="standard" required />
-								<TextField label="Phone number" type="text" name="phone_number" variant="standard" required />
-							</FormGroup>
+						<FormGroup>
+							<div style={{ display: "flex" }}>
+								<TextField label="First name" type="text" name="first_name" variant="standard" required sx={{ flex: 1 }} />
+								<TextField label="Last name" type="text" name="last_name" variant="standard" required sx={{ flex: 1 }} />
+							</div>
+							<TextField label="Email" type="email" name="email" variant="standard" required />
+							<TextField label="Phone number" type="text" name="phone_number" variant="standard" required />
+						</FormGroup>
 
-							<FormGroup>
-								<TextField label="Street name" type="text" name="street" variant="standard" required />
-								<TextField label="House nr" type="text" name="house_number" variant="standard" required />
-								<TextField label="Zip code" type="text" name="zip_code" variant="standard" required />
-								<TextField label="Provice" type="text" name="province" variant="standard" required />
-								<TextField label="City" type="text" name="city" variant="standard" required />
-								<TextField label="Country" type="text" name="country" variant="standard" required />
-							</FormGroup>
-						</FormControl>
+						<FormGroup sx={{ mt: 3 }}>
+							<TextField label="Street name" type="text" name="street" variant="standard" required />
+							<TextField label="House nr" type="text" name="house_number" variant="standard" required />
+							<TextField label="Zip code" type="text" name="zip_code" variant="standard" required />
+							<TextField label="Provice" type="text" name="province" variant="standard" required />
+							<TextField label="City" type="text" name="city" variant="standard" required />
+							<TextField label="Country" type="text" name="country" variant="standard" required />
+						</FormGroup>
+						<FormGroup>
+							<Button>Order</Button>
+
+						</FormGroup>
+
+
 					</form>
+				</Grid>
+				<Grid item xs={5}>
+					Auto hier
 				</Grid>
 			</Grid>
 		</>
