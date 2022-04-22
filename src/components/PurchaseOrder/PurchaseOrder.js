91c84d742b3d6816aa660dcf887f8d13f4d86009
Servi Huijbregts <346215@student.fontys.nl>
2022-04-22 10:12:09 +0200

    Purchase order table styled with image and real data

--- src/components/PurchaseOrder/PurchaseOrder.js
+++ src/components/PurchaseOrder/PurchaseOrder.js
@@ -1,35 +1,49 @@
 import "./purchaseorder.css"
 import { useNavigate } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
-import { Card, Grid } from "@mui/material";
+import {Button, Card, Grid} from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-
-const columns = [
-	{ field: "brand_name", headerName: "Brand", width: 250 },
-	{ field: "model_name", headerName: "Model", width: 250 },
-];
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
 
 export default function PurchaseOrder() {
-	// const { data } = useFetch("http://localhost:8087/api/v1/purchase-order/dealer/orders", "company_ABC");
-
+	const { data } = useFetch("http://localhost:8086/api/v1/purchase_order/company_ABC");
+	const columns = [
+		{ field: "image_url", headerName: "Preview",  width: 150,
+			renderCell: (params) => <img className={"car_image"} src={params.value}  alt={"Car"}/>},
+		{ field: "brand", headerName: "Brand", width: 150 },
+		{ field: "model", headerName: "Model", width: 200 },
+		{ field: "color", headerName: "Color", width: 200 },
+		{ field: "delivery_price", headerName: "Delivery Price", width: 150 },
+		{ field: "delivery_date", headerName: "Delivery Date", width: 150 },
+		{ field: "created_time", headerName: "Request made on", width: 150 },
+		{ field: "uuid", headerName: "Order Ready", width: 150,
+			renderCell: (params) =>
+				<div>
+					<Button className={"buttonStyle"} variant="outlined" onClick={() => {onPurchaseOrderClick(params)}}>
+						<CheckBoxIcon/>
+					</Button>
+				</div>
+		},
+	];
 	const navigate = useNavigate();
 	function onPurchaseOrderClick(uuid) {
-		navigate(`/purchase-order/${uuid}`)
+		navigate(`/purchase-order/${uuid.formattedValue}`)
 	}
 
-	// const arr = [];
-	// data.map((item) =>
-	// 	arr.push({
-	// 		uuid: item.uuid,
-	// 		delivery_date: new Date(item.delivery_date * 1000),
-	// 		delivery_price: `€ ${item.delivery_price}`,
-	// 		model_name: item.model_name,
-	// 		purchase_order_uuid: item.purchase_order_uuid,
-	// 		brand_name: item.brand_name,
-	// 	})
-	// );
-
-	const arrayData = [{"uuid": "uuid_a", "brand_name": "BMW", "model_name": "M3"}];
+	const arr = [];
+	data.map((item) =>
+		arr.push({
+			uuid: item.uuid,
+			created_time: new Date(item.created_time * 1000).toLocaleDateString(),
+			delivery_date: new Date(item.delivery_date * 1000).toLocaleDateString(),
+			delivery_price: `€ ${item.delivery_price}`,
+			model: item.model,
+			price: item.price.toString(),
+			brand: item.brand,
+			image_url: item.image_url,
+			color: item.color,
+		})
+	);
 
 
 
@@ -41,12 +55,11 @@ export default function PurchaseOrder() {
 					<div style={{ height: 400, width: "100%" }}>
 						<DataGrid
 							getRowId={(row) => row.uuid}
-							rows={arrayData}
+							rows={arr}
 							columns={columns}
 							pageSize={5}
 							rowsPerPageOptions={[5]}
 							components={{ Toolbar: GridToolbar }}
-							onRowClick={(row) => { onPurchaseOrderClick(row.id) }}
 						/>
 					</div>
 				</Grid>
